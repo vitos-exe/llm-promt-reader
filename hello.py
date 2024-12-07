83dab9b13c34179a395dad4276fa3d4913f5b595
Vitalii Chernysh <vivich4omega@gmail.com>
2024-12-07 20:07:57 +0100

    WIP

--- hello.py
+++ hello.py
@@ -1,24 +1,44 @@
 from litellm import completion
 import gradio as gr
+from retriever import Retriever
 
 
 MODEL_NAME = 'ollama/llama3.2'
 API_BASE = 'http://localhost:11434'
 SYSTEM_MESSAGE = {
     'role': 'system', 
-    'content': 'You are an assistant that rewrites text to make it more formal and professional while preserving the original meaning.'
+    'content': 
+    '''
+    You are given some context and also a question. Answer the question using given context.
+    '''
 }
 
-def answer_question(question: str):
-    answer = completion(
-        model=MODEL_NAME,
-        api_base=API_BASE,
-        messages=[SYSTEM_MESSAGE, {'role': 'user', 'content': question}]
-    )
-    return answer.json()['choices'][0]['message']['content']
+class RAGQuestionAnsweringBot:
+    def __init__(self, docs: list[str]) -> None:
+        self.retriever = Retriever(docs)
+
+    def answer_question(self, question: str):
+        context = self.retriever.get_docs(question)
+
+        messages = [
+            SYSTEM_MESSAGE,
+            {'role': 'user', 'content': f'Context:\n{context}\nQuestion: {question}'}
+        ]
+
+        answer = completion(
+            model=MODEL_NAME,
+            api_base=API_BASE,
+            messages=messages
+        )
+        return answer.json()['choices'][0]['message']['content']
 
 
 if __name__ == "__main__":
-    demo = gr.Interface(fn=answer_question, inputs='text', outputs='text')
-    demo.launch();
+    bot = RAGQuestionAnsweringBot([])
+    query = 'How are you doing?'
+    answer = bot.answer_question(query)
+    print(answer)
+
+    # demo = gr.Interface(fn=answer_question, inputs='text', outputs='text')
+    # demo.launch();
 
---
+++ retriever.py
@@ -0,0 +1,8 @@
+class Retriever:
+    def __init__(self, docs: list[str]):
+        self.docs = docs
+        pass
+
+    def get_docs(self, query, n=3) -> list[str]:
+        return []
+
